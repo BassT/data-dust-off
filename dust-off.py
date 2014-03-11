15e8b87440cf3a3f3e19d62a403822221eee60c1
BassT <sebastian.richter@student.kit.edu>
2014-03-10 21:46:23 -0400

    finished numberAsStringToNumberAsNumber

--- dust-off.py
+++ dust-off.py
@@ -20,6 +20,7 @@ def import_col(file):
 	col = raw_input("Enter the name of the column you want to select: ").upper()
 	start = int(raw_input("Enter the first row you want to include in the selection: "))
 	end = int(raw_input("Enter the last row you want to include in the selection: "))
+	print "The values of your selection:"
 	for i in range(start, end):
 		print ws[col + str(i)].value
 		data.append(ws[col + str(i)].value)
@@ -39,9 +40,38 @@ def convert_string_typed_number_to_number_typed_number(data_in):
 	data_out = []
 
 	for i in range(0,len(data_in)):
-		data_out.append(float(re.sub(r'\s*\$*\s*([0-9]{0,3}),*([0-9]{0,3}),*([0-9]{1,3})(\.[0-9]+)\s*', r'\1\2\3\4', data(i))))
+		print "\nConverting data[" + str(i) + "]: " + str(data_in[i])
+		if (data_in[i] == None): # missing value
+			data_out.append(None)
+			print "Converted data [" + str(i) + "] to: missing value"
+		else:
+			try: # let's try to convert the string
+				converted_number = float(re.sub(r'\s*\$*\s*([0-9]{0,3}),*([0-9]{0,3}),*([0-9]{1,3})(\.[0-9]+)\s*', r'\1\2\3\4', str(data_in[i])))
+				if (converted_number == 0): # figure out how to handle zeros
+					keep_zero = raw_input("I read a zero. Do you want to keep the zero (type 0) or convert it to a missing value (leave blank)?")
+					if(keep_zero == ""):
+						converted_number = None
+				data_out.append(converted_number)
+				print "Converted data[" + str(i) + "] to: " + str(converted_number) + " (type: " + str(type(converted_number)) + ")"
+			except ValueError: # couldn't convert string - ask a human for help
+				not_converted = True
+				print "Could not convert '" + str(data_in[i]) + "' to a number"
+				while(not_converted): # for loop until we have a valid input
+					manual_input = raw_input("Please enter a number or leave blank for a missing value: ")
+					if (manual_input == None or manual_input == ""): # set to missing value
+						data_out.append(None)
+						not_converted = False
+						print "Converted data[" + str(i) + "] to: missing value"
+					else: # try to convert entered input
+						try: 
+							data_out.append(float(manual_input))
+							print "Manually converted data[" + str(i) + "] to: " + manual_input
+							not_converted = False
+						except ValueError:
+							pass
 
 	return data_out
 
 data = import_col("/home/sebastian/Documents/Datathon TO/MASTER - Program Tracking SheetATMarch2014.xlsx")
+data = convert_string_typed_number_to_number_typed_number(data)
 print "Success"
